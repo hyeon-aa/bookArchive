ec85dc1631bcfeb118b0db79d5b511ed08fb029d
hyeon-aa <ahhyun1120@naver.com>
2026-01-17 10:04:56 +0900

    feat(front): 외부 이미지 설정 추가

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+const NAVER_IMAGE_HOSTS = [
+  "bookthumb-phinf.pstatic.net",
+  "shopping-phinf.pstatic.net",
+];
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  images: {
+    remotePatterns: NAVER_IMAGE_HOSTS.map((host) => ({
+      protocol: "https",
+      hostname: host,
+    })),
+  },
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
