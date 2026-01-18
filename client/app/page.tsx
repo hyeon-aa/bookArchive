e0628a84b594e7247099b850790c955b9839d194
hyeon-aa <ahhyun1120@naver.com>
2026-01-18 20:45:04 +0900

    feat(front): 홈화면 검색화면으로 redirect

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function HomePage() {
-  return (
-    <h1 className="text-2xl font-bold text-gray-800">
-      안녕하세요, <span className="text-blue-600">반가워요</span>
-    </h1>
-  );
+  redirect("/books/search");
 }
