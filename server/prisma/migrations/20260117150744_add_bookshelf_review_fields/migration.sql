40c41b6a38f4acdb633068ce002bfb1e9d8f7ffd
hyeon-aa <ahhyun1120@naver.com>
2026-01-18 20:41:28 +0900

    feat(back): ai 태그 및 독서기록 관련 칼럼 추가

---
+++ server/prisma/migrations/20260117150744_add_bookshelf_review_fields/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Bookshelf" ADD COLUMN     "comment" TEXT,
+ADD COLUMN     "emotion" TEXT,
+ADD COLUMN     "endDate" TIMESTAMP(3),
+ADD COLUMN     "startDate" TIMESTAMP(3);
