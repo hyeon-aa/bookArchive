cf66b9d5b99e3ca4de10514c7a50e70816910511
hyeon-aa <ahhyun1120@naver.com>
2026-03-18 23:28:03 +0900

    feat(front): 결제 취소, 내 결제 조회 api 연동

--- client/feature/payment/api.ts
+++ client/feature/payment/api.ts
@@ -1,7 +1,10 @@
 import { api } from "@/lib/api";
 import {
+  CancelPaymentRequest,
   ConfirmPaymentRequest,
+  myPaymentsResponse,
   PaymentResponse,
+  TossCancelResponse,
   TossConfirmResponse,
 } from "./type";
 
@@ -13,6 +16,16 @@ export const paymentApi = {
 
   confirm: async (dto: ConfirmPaymentRequest) => {
     const data = await api.post<TossConfirmResponse>("/payments/confirm", dto);
+    return data;
+  },
+
+  cancel: async (dto: CancelPaymentRequest) => {
+    const data = await api.post<TossCancelResponse>("payments/cancel", dto);
+    return data;
+  },
+
+  getMyPayments: async () => {
+    const data = await api.get<myPaymentsResponse[]>("payments/me");
     return data;
   },
 };
